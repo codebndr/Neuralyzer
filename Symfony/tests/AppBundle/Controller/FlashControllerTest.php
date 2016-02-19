f162f7d3157b945846a01eae8f7a1e4aeed70130
Alex Yao <alexyaoyang@hotmail.com>
2016-02-20 02:01:58 +0800

    Rename and edit flash test file for new url

--- Symfony/tests/AppBundle/Controller/FlashControllerTest.php
+++ Symfony/tests/AppBundle/Controller/FlashControllerTest.php
@@ -4,13 +4,13 @@ namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class FlashControllerTest extends WebTestCase
 {
     public function testIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/flash');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Upload .hex file', $crawler->filter('h2')->text());
