f2066a8b66464733cfb35a754abcdb5907e7fafe
Alex Yao <alexyaoyang@hotmail.com>
2016-02-20 18:42:46 +0800

    Add more unit tests for flash page

--- Symfony/tests/AppBundle/Controller/DefaultControllerTest.php
+++ Symfony/tests/AppBundle/Controller/DefaultControllerTest.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testUploadPage()
     {
         $client = static::createClient();
 
@@ -14,5 +14,25 @@ class DefaultControllerTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Upload .hex file', $crawler->filter('h2')->text());
+    }
+
+    public function testConfigPage()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Select Board', $crawler->filter('h2')->eq(2)->text());
+    }
+
+    public function testFlashPage()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Select Button Name', $crawler->filter('h2')->eq(5)->text());
     }
 }
