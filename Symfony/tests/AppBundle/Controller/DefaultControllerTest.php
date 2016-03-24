bb02606b0100e55b866f384b60c0b543e0685fb1
Alex Yao <alexyaoyang@hotmail.com>
2016-03-24 21:25:11 +0800

    Merge and Improve tests

--- Symfony/tests/AppBundle/Controller/DefaultControllerTest.php
+++ Symfony/tests/AppBundle/Controller/DefaultControllerTest.php
@@ -14,25 +14,7 @@ class DefaultControllerTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Upload .hex file', $crawler->filter('h2')->text());
-    }
-
-    public function testConfigPage()
-    {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Select Board', $crawler->filter('h2')->eq(2)->text());
-    }
-
-    public function testFlashPage()
-    {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Select Button Name', $crawler->filter('h2')->eq(5)->text());
+        $this->assertContains('Next', $crawler->filter('button#nextButton')->text());
+        $this->assertContains('Submit', $crawler->filter('button#submitButton')->text());
     }
 }
