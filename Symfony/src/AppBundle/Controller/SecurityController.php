e780f79882b7ea3bc2aec028affe2dc45a77207a
Alex Yao <alexyaoyang@hotmail.com>
2016-03-25 02:45:35 +0800

    Remove comments

--- Symfony/src/AppBundle/Controller/SecurityController.php
+++ Symfony/src/AppBundle/Controller/SecurityController.php
@@ -14,16 +14,13 @@ class SecurityController extends Controller
     {
         $authenticationUtils = $this->get('security.authentication_utils');
 
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render(
             'AppBundle:Security:index.html.twig',
             array(
-                // last username entered by the user
                 'last_username' => $lastUsername,
                 'error' => $error,
             )
