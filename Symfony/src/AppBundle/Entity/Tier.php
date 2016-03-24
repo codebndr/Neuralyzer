85816ea3a639e735b2ef28d5f302399d019c8ad8
Alex Yao <alexyaoyang@hotmail.com>
2016-03-25 00:32:22 +0800

    Make user and tier phpmd suppress more specific

--- Symfony/src/AppBundle/Entity/Tier.php
+++ Symfony/src/AppBundle/Entity/Tier.php
@@ -9,7 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
  *
  * @ORM\Entity(repositoryClass="AppBundle\Repository\TierRepository")
  *
- * @SuppressWarnings(PHPMD)
+ * @SuppressWarnings(PHPMD.ShortVariable)
  */
 class Tier
 {
--- Symfony/src/AppBundle/Entity/User.php
+++ Symfony/src/AppBundle/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  *
  * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
  *
- * @SuppressWarnings(PHPMD)
+ * @SuppressWarnings(PHPMD.ShortVariable)
  */
 class User implements UserInterface, \Serializable
 {
