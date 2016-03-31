f8460addb3515e49adcc6bae4e3be19f5af55ea2
Alex Yao <alexyaoyang@hotmail.com>
2016-04-01 02:11:11 +0800

    Merge show tier page branch

---
+++ Symfony/src/AppBundle/Controller/TierController.php
@@ -0,0 +1,23 @@
+<?php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\Tier;
+use AppBundle\Entity\User;
+use AppBundle\Entity\FirmwareConfig;
+
+class TierController extends Controller
+{
+    /**
+     * @Route("/tier", name="tier")
+     */
+    public function showAction()
+    {
+        $tiers = $this->getDoctrine()
+            ->getRepository('AppBundle:Tier')
+            ->findAll();
+
+        return $this->render('AppBundle:Tier:index.html.twig', array('tiers' => $tiers));
+    }
+}
